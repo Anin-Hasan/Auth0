98e00d44bf2e450f45aff9cbf3c65143f16a83d7
Anin-Hasan <200921@ku.ac.bd>
2023-07-10 19:45:08 +0600

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,8 @@ import App from "./App.jsx";
 import "./index.css";
 import { Auth0Provider } from "@auth0/auth0-react";
 
-const domain = import.meta.env.VITE_REACT_APP_AUTH0_DOMAIN;
-const clientId = import.meta.env.VITE_REACT_APP_AUTH0_CLIENT_ID;
+const domain = "dev-jhadivt2el7i44wt.us.auth0.com";
+const clientId = "0TY7riaBkHwNlaON1Beu6i6CutBFKbG7";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
